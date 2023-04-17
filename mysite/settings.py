621cdfb9201aad7f37508f7c4a6784daf4d56576
Djura94 <fdjuric@gmail.com>
2023-04-17 19:09:24 +0200

    installed whitenoise

--- mysite/settings.py
+++ mysite/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -159,3 +160,5 @@ DATABASES = {
     'default':dj_database_url.parse(env('DATABASE_URL'))
 
 }
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
