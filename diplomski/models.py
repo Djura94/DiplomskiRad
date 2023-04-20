291c5b749acca185a881385120bacc1b3d10e4c1
Djura94 <fdjuric@gmail.com>
2023-04-20 15:40:06 +0200

    different solution for images

--- diplomski/models.py
+++ diplomski/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 
 class Course(models.Model):
-    image = models.ImageField(upload_to="images/")
+    image = models.ImageField()
     summary=models.CharField(max_length=200)
     description=models.CharField(max_length=400, default='')
     profesor_link=models.CharField(max_length=100, default='')
