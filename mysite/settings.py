c81814dcb8e9561632f37b4cfa044c6d2dddb029
Djura94 <fdjuric@gmail.com>
2023-04-20 12:35:29 +0200

    fixing some routes

--- mysite/settings.py
+++ mysite/settings.py
@@ -116,7 +116,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = 'https://ik.imagekit.io/acripec8h/images/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'https://ik.imagekit.io/acripec8h/images/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
